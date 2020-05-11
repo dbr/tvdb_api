ccdf0077252976632dd566594670cbaa868bf3d8
dbr <dbr.onix@gmail.com>
2020-05-11 12:14:52 +0930

    Remove deprecated "setup.py test"

--- setup.py
+++ setup.py
@@ -1,25 +1,5 @@
 import sys
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 
 _requirements = ['requests_cache', 'requests']
@@ -50,9 +30,6 @@ u'Stole a Badge'
 
 py_modules = _modules,
 install_requires = _requirements,
-
-tests_require=['pytest'],
-cmdclass = {'test': PyTest},
 
 classifiers=[
     "Intended Audience :: Developers",
